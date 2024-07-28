54d82f9e276ea166007d73aa06af0b7a3a0826be
Karish-Kareem2003 <karishmakareem85@gmail.com>
2024-07-28 09:15:17 +0530

    Add files via upload

---
+++ CALCULATOR PROGRAM/calciout.py
@@ -0,0 +1,62 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Samyu/Desktop/calci.py
+Enter the first number: 45
+Enter the second number: 23
+Choose an operation:
+1. Addition (+)
+2. Subtraction (-)
+3. Multiplication (*)
+4. Division (/)
+5. Modulus (%)
+Enter the operation (1/2/3/4/5): 1
+The result of 45 + 23 is: 68
+
+============================================================= RESTART: C:/Users/Samyu/Desktop/calci.py ============================================================
+Enter the first number: 87
+Enter the second number: 43
+Choose an operation:
+1. Addition (+)
+2. Subtraction (-)
+3. Multiplication (*)
+4. Division (/)
+5. Modulus (%)
+Enter the operation (1/2/3/4/5): 2
+The result of 87 - 43 is: 44
+>>> 
+============================================================= RESTART: C:/Users/Samyu/Desktop/calci.py ============================================================
+Enter the first number: 654
+Enter the second number: 6
+Choose an operation:
+1. Addition (+)
+2. Subtraction (-)
+3. Multiplication (*)
+4. Division (/)
+5. Modulus (%)
+Enter the operation (1/2/3/4/5): 3
+The result of 654 * 6 is: 3924
+>>> 
+============================================================= RESTART: C:/Users/Samyu/Desktop/calci.py ============================================================
+Enter the first number: 76
+Enter the second number: 2
+Choose an operation:
+1. Addition (+)
+2. Subtraction (-)
+3. Multiplication (*)
+4. Division (/)
+5. Modulus (%)
+Enter the operation (1/2/3/4/5): 4
+The result of 76 / 2 is: 38.0
+>>> 
+============================================================= RESTART: C:/Users/Samyu/Desktop/calci.py ============================================================
+Enter the first number: 350
+Enter the second number: 5
+Choose an operation:
+1. Addition (+)
+2. Subtraction (-)
+3. Multiplication (*)
+4. Division (/)
+5. Modulus (%)
+Enter the operation (1/2/3/4/5): 5
+The result of 350 % 5 is: 0
